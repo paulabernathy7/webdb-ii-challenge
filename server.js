b61b54015418ed609adf43b2fcd2f396bbb7b146
Paul Abernathy <paul.abernathy7@gmail.com>
2019-08-11 22:43:48 -0400

    it works now

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
-server.use("api/cars", carsRouter);
+server.use("/api/cars", carsRouter);
+
+server.get("/", (req, res) => {
+  res.send("<h3>DB Helpers with knex</h3>");
+});
 
 module.exports = server;
 // create knex file using knex init
